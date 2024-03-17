277d931128967edb72ba34182b3f75493cd718fc
Adnan-Najjar <161934494+Adnan-Najjar@users.noreply.github.com>
2024-03-17 04:59:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,27 @@
+#run: streamlit run streamlit_app.py
+
 import google.generativeai as genai
 import streamlit as st
 from PyPDF2 import PdfReader
 
+# Start gemini model and chat
+genai.configure(api_key='AIzaSyBKQ_tbS34bR0Z6MM7j2iE5T4sEkZlM98k')
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat()
+
+def send(message):
+    try:
+        return chat.send_message(message).text
+    except:
+        st.warning("Query Blocked!", icon=':warning:')
+
 def main():
     # Set the page title and layout
     st.set_page_config(page_title="Gemini ChatBot",
-                    page_icon="🤖",
+                    page_icon=":robot_face:",
                     layout="wide",
                     )
     st.title(':left_speech_bubble: :blue[AI ChatBot using Gemini] :left_speech_bubble:')
-    
-    # Start gemini Chat
-    genai.configure(api_key='AIzaSyBKQ_tbS34bR0Z6MM7j2iE5T4sEkZlM98k')
-    model = genai.GenerativeModel('gemini-pro')
-    chat = model.start_chat()
 
     uploaded_file = st.file_uploader("Upload file ", type="pdf")
     user_input = st.chat_input("What is up?")
@@ -29,35 +37,38 @@ def main():
 
     # Read the PDF and send it to the AI
     if uploaded_file:
-        with st.spinner('Loading...'):
+        with st.spinner('Loading the file...'):
             pdf_reader = PdfReader(uploaded_file)
             text = ""
             for page in pdf_reader.pages:
-                text += page.extract_text()
-            chat.send_message(text)
-            st.success('Done!')
-            st.balloons()
+                if page.extract_text():
+                    text += page.extract_text()
+                else:
+                    text += "Empty page"
+            send(text)
+        st.success('File Loaded!', icon=':white_check_mark:')
+        st.balloons()
     
     if user_input:
-        # User chat
-        with st.chat_message("user"):
-            # Display input
-            st.markdown(user_input)
-            
-            # Add to history
-            st.session_state.messages.append({"role": "user", "content": user_input})
-            
-            # Create response
-            response = f"ChatBot:\n {chat.send_message(user_input).text}"
+        with st.spinner('Responding...'):
+            # User chat
+            with st.chat_message("user"):
+                # Display input
+                st.markdown(user_input)
+                
+                # Add to history
+                st.session_state.messages.append({"role": "user", "content": user_input})
+                
+                # Create response
+                response = f"ChatBot:\n {send(user_input)}"
 
-        # ChatBot chat
-        with st.chat_message("assistant"):
-            # Display response
-            st.markdown(response)
-            
-            # Add to history
-            st.session_state.messages.append({"role": "assistant", "content": response})
-
+            # ChatBot chat
+            with st.chat_message("assistant"):
+                # Display response
+                st.markdown(response)
+                
+                # Add to history
+                st.session_state.messages.append({"role": "assistant", "content": response})
 try:
     main()
 except Exception as e:
