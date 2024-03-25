8fe183acf54781ffe743dd38906c0ccf17217420
Adnan-Najjar <161934494+Adnan-Najjar@users.noreply.github.com>
2024-03-25 03:54:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,59 +1,61 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain_community.vectorstores.faiss import FAISS
-from langchain.chains.question_answering import load_qa_chain
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain.prompts import PromptTemplate
 
 # Google api key
 GOOGLE_API_KEY = 'AIzaSyBKQ_tbS34bR0Z6MM7j2iE5T4sEkZlM98k'
 
 # model for normal chatbot
 genai.configure(api_key=GOOGLE_API_KEY)
-model0 = genai.GenerativeModel(model_name="gemini-pro")
+model = genai.GenerativeModel(model_name="gemini-pro")
+chat = model.start_chat(history=[])
 
-# model for documents chatbot
-model = ChatGoogleGenerativeAI(google_api_key=GOOGLE_API_KEY, model="gemini-pro")
+def load_pdf(file):
+    pdf_reader = PdfReader(file)
+    text = ""
+    for page in pdf_reader.pages:
+        if page.extract_text():
+            text += page.extract_text() + " "
 
-def get_conversational_chain():
+    chunks = RecursiveCharacterTextSplitter(chunk_size=1000,
+                                            chunk_overlap=200).split_text(text)
+    return chunks
 
-    prompt_template = """
-    Be a helpful assistant and help with this pdf file and be open for any questions about it\n\n
-    Context:\n {context}?\n
-    Question: \n{question}\n
-
-    Answer:
-    """
-
-    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
-
-    return chain
-
-def get_response(user_question,file):
+def get_response(user_question):
+    # use the file in the AI if the query starts with 'file:'
     if user_question.startswith("file:"):
-        pdf_reader = PdfReader(file)
-        text = ""
-        for page in pdf_reader.pages:
-            if page.extract_text():
-                text += page.extract_text()
+        chunks = st.session_state.file
         
-        chunks = RecursiveCharacterTextSplitter(chunk_size=1000,
-                                                chunk_overlap=200).split_text(text)
+        # for small files use it as a whole
+        if len(chunks)<5:
+            context = '\n'.join(chunks)
+        # for big files use embeddings and vector store to get chunks relevant to the question
+        else:
+            embeddings = GoogleGenerativeAIEmbeddings(google_api_key=GOOGLE_API_KEY, model = "models/embedding-001")
+            vector_store = FAISS.from_texts(chunks, embeddings)
+            docs = vector_store.similarity_search(user_question)
+            context = ""
+            for doc in docs:
+                context += doc.page_content + " "
 
-        embeddings = GoogleGenerativeAIEmbeddings(google_api_key=GOOGLE_API_KEY, model = "models/embedding-001")
-        vector_store = FAISS.from_texts(chunks, embeddings)
+        response = chat.send_message(f"""Answer the question based on the context: 
+                                     Context: {context}
+                                     Question: {user_question} 
+                                     """).text
         
-        docs = vector_store.similarity_search(user_question)
-        chain = get_conversational_chain()
-        response = chain.run(input_documents=docs, question=user_question)
         return response
+    # otherwise use the AI normally
     else:
-        response = model0.generate_content(user_question).text
+        response = chat.send_message(user_question).text
         return response
-    
+
+# changes state
+def isNew():
+    st.session_state.is_new_file = True
+
 def main():
 
     # Set the page title and layout
@@ -63,19 +65,37 @@ def main():
                     )
     st.title(':rainbow[AI ChatBot using Gemini] :left_speech_bubble:')
 
-    uploaded_file = st.file_uploader("Upload file ", type="pdf")
-    user_input = st.chat_input("What is up?")
-    st.write("Write 'file:' to access the file content")
+    # Init boolean variable to check if there is a new file
+    if "is_new_file" not in st.session_state:
+        st.session_state.is_new_file = False
 
+    if "file" not in st.session_state:
+        st.session_state.file = None
+    
     # Init chat history
     if "messages" not in st.session_state:
         st.session_state.messages = []
     
+    uploaded_file = st.file_uploader("Upload file ", type="pdf", on_change=isNew)
+    user_input = st.chat_input("What is up?")
+    st.write("Write \" file: \" to access the file content")
+
     # Display messagess
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
+
+    # Loading a new file
+    if st.session_state.is_new_file and uploaded_file:
+        with st.spinner("Loading File..."):
+            
+            st.session_state.file = load_pdf(uploaded_file) 
+            st.session_state.is_new_file = False
+            
+            st.success("File Loaded!",icon="✅")
+            st.balloons()
     
+
     if user_input:
         with st.spinner('Responding...'):
             # User chat
@@ -88,7 +108,7 @@ def main():
                 
                 # Create response
                 try:
-                    response = get_response(user_input,uploaded_file)
+                    response = get_response(user_input)
                 except:
                     response = "Error getting the response"
             
